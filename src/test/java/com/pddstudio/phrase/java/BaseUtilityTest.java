f373c2f3332bc89034efcb3a5381450bbbdf0093
Patrick Jung <patrick.jung@arconsis.com>
2016-10-15 15:36:19 +0200

    Separated test utility methods from actual test class

---
+++ src/test/java/com/pddstudio/phrase/java/BaseUtilityTest.java
@@ -0,0 +1,135 @@
+package com.pddstudio.phrase.java;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by pddstudio on 15/10/2016.
+ */
+public abstract class BaseUtilityTest {
+
+	private int    methodNameCount = 20;
+	private Random random          = new Random();
+
+	protected void printResult(String methodName, String result, String expected) {
+		StringBuilder message = new StringBuilder();
+		int remainingWhitespaces = methodNameCount - methodName.length();
+		if(remainingWhitespaces < 0) {
+			remainingWhitespaces = 0;
+			message.append("[EXPECTED] : ").append(methodName.substring(0, methodNameCount -3)).append("...");
+		} else {
+			message.append("[EXPECTED] : ").append(methodName);
+		}
+		for(int i = 0; i < remainingWhitespaces; i++) {
+			message.append(" ");
+		}
+		message.append(" > ").append(expected);
+		System.out.println(message.toString());
+		//print the actual result too
+		printResult(methodName, result);
+	}
+
+	private void printResult(String methodName, String result) {
+		StringBuilder message = new StringBuilder();
+		int remainingWhitespaces = methodNameCount - methodName.length();
+		if(remainingWhitespaces < 0) {
+			remainingWhitespaces = 0;
+			message.append("[RESULT  ] : ").append(methodName.substring(0, methodNameCount -3)).append("...");
+		} else {
+			message.append("[RESULT  ] : ").append(methodName);
+		}
+		for(int i = 0; i < remainingWhitespaces; i++) {
+			message.append(" ");
+		}
+		message.append(" > ").append(result);
+		System.out.println(message.toString());
+	}
+
+	private <T> String getExpectedArrayResult(T[] values, String separator) {
+		//falling back to the default separator
+		if (separator == null || separator.length() == 0) {
+			separator = " ";
+		}
+		StringBuilder sb = new StringBuilder();
+		List<T> list = Arrays.asList(values);
+		Iterator<T> iterator = list.iterator();
+		while(iterator.hasNext()) {
+			sb.append(String.valueOf(iterator.next()));
+			if(iterator.hasNext()) {
+				sb.append(separator);
+			}
+		}
+		return sb.toString();
+	}
+
+	protected int[] getRandomIntArray() {
+		int size = random.nextInt(100);
+		int[] ints = new int[size];
+		for(int i = 0; i < ints.length; i++) {
+			ints[i] = random.nextInt(Integer.MAX_VALUE);
+		}
+		return ints;
+	}
+
+	protected float[] getRandomFloatArray() {
+		int size = random.nextInt(100);
+		float[] floats = new float[size];
+		for(int i = 0; i < floats.length; i++) {
+			floats[i] = random.nextFloat();
+		}
+		return floats;
+	}
+
+	protected double[] getRandomDoubleArray() {
+		int size = random.nextInt(100);
+		double[] doubles = new double[size];
+		for(int i = 0; i < doubles.length; i++) {
+			doubles[i] = random.nextDouble();
+		}
+		return doubles;
+	}
+
+	protected boolean[] getRandomBooleanArray() {
+		int size = random.nextInt(100);
+		boolean[] bools = new boolean[size];
+		for(int i = 0; i < bools.length; i++) {
+			bools[i] = random.nextBoolean();
+		}
+		return bools;
+	}
+
+	protected String getExpectedIntArrayResult(int[] values, String separator) {
+		Integer[] integers = new Integer[values.length];
+		for(int i = 0; i < integers.length; i++) {
+			integers[i] = values[i];
+		}
+		return getExpectedArrayResult(integers, separator);
+	}
+
+	protected String getExpectedFloatArrayResult(float[] values, String separator) {
+		Float[] floats = new Float[values.length];
+		for(int i = 0; i < floats.length; i++) {
+			floats[i] = values[i];
+		}
+		return getExpectedArrayResult(floats, separator);
+	}
+
+	protected String getExpectedDoubleArrayResult(double[] values, String separator) {
+		Double[] doubles = new Double[values.length];
+		for(int i = 0; i < doubles.length; i++) {
+			doubles[i] = values[i];
+		}
+		return getExpectedArrayResult(doubles, separator);
+	}
+
+	protected String getExpectedBooleanArrayResult(boolean[] values, String separator) {
+		Boolean[] bools = new Boolean[values.length];
+		for(int i = 0; i < bools.length; i++) {
+			bools[i] = values[i];
+		}
+		return getExpectedArrayResult(bools, separator);
+	}
+
+}
--- src/test/java/com/pddstudio/phrase/java/PhraseTest.java
+++ src/test/java/com/pddstudio/phrase/java/PhraseTest.java
@@ -3,51 +3,42 @@ package com.pddstudio.phrase.java;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by pddstudio on 15/10/2016.
  */
-public class PhraseTest {
+public class PhraseTest extends BaseUtilityTest {
 
-	private  int methodNameCount = 20;
-
-	private void printResult(String methodName, String result) {
-		StringBuilder message = new StringBuilder();
-		int remainingWhitespaces = methodNameCount - methodName.length();
-		if(remainingWhitespaces < 0) {
-			remainingWhitespaces = 0;
-			message.append("[RESULT] : ").append(methodName.substring(0, methodNameCount -3)).append("...");
-		} else {
-			message.append("[RESULT] : ").append(methodName);
-		}
-		for(int i = 0; i < remainingWhitespaces; i++) {
-			message.append(" ");
-		}
-		message.append(" > ").append(result);
-		System.out.println(message.toString());
-	}
+	private final String separator = " | ";
 
 	@Test
 	public void helloWorldTest() {
 		String greeting = "Hello";
 		String greetingTarget = "World";
+		String expected = "Hello World!";
 		String phrase = Phrase.from("{greeting} {who}!").put("greeting", greeting).put("who", greetingTarget).formatString();
-		printResult("helloWorldTest()", phrase);
-		assertEquals(phrase, "Hello World!");
+		printResult("helloWorldTest()", phrase, expected);
+		assertEquals(phrase, expected);
 	}
 
 	@Test
 	public void bigDecimalTest() {
 		BigDecimal firstNumber = new BigDecimal(500);
 		BigDecimal secondNumber = new BigDecimal(3);
+		String expected = "500 / 3 = 167";
 		String phrase = Phrase.from("{first} / {second} = {result}")
 							  .put("first", firstNumber)
 							  .put("second", secondNumber)
 							  .put("result", firstNumber.divide(secondNumber, BigDecimal.ROUND_HALF_DOWN)).formatString();
-		printResult("bigDecimalTest()", phrase);
-		assertEquals(phrase, "500 / 3 = 167");
+		printResult("bigDecimalTest()", phrase, expected);
+		assertEquals(phrase, expected);
 	}
 
 	@Test
@@ -56,13 +47,14 @@ public class PhraseTest {
 		String name = "pddstudio";
 		String action = "isAwesome";
 		boolean actionValue = true;
+		String expected = "https://github.com/pddstudio/?isAwesome=true";
 		String phrase = Phrase.from("{host}/{name}/?{action}={action_value}")
 							  .put("host", host)
 							  .put("name", name)
 							  .put("action", action)
 							  .put("action_value", actionValue).formatString();
-		printResult("dummyRestUrlTest()", phrase);
-		assertEquals(phrase, "https://github.com/pddstudio/?isAwesome=true");
+		printResult("dummyRestUrlTest()", phrase, expected);
+		assertEquals(phrase, expected);
 	}
 
 	@Test
@@ -70,13 +62,50 @@ public class PhraseTest {
 		int intValue = 471;
 		float floatValue = 21.9F;
 		double doubleValue = 1921.2;
+		String expected = "Integer: 471 | Float: 21.9 | Double: 1921.2";
 		String phrase = Phrase.from("Integer: {integer} | Float: {float} | Double: {double}")
 							  .put("integer", intValue)
 							  .put("float", floatValue)
 							  .put("double", doubleValue)
 							  .formatString();
-		printResult("digitsTest()", phrase);
-		assertEquals(phrase, "Integer: 471 | Float: 21.9 | Double: 1921.2");
+		printResult("digitsTest()", phrase, expected);
+		assertEquals(phrase, expected);
+	}
+
+	@Test
+	public void intArrayTest() {
+		int[] ints = getRandomIntArray();
+		String expected = getExpectedIntArrayResult(ints, separator);
+		String phrase = Phrase.from("{integer_array}").putArray("integer_array", ints, separator).formatString();
+		printResult("intArrayTest()", phrase, expected);
+		assertEquals(phrase, expected);
+	}
+
+	@Test
+	public void booleanArrayTest() {
+		boolean[] bools = getRandomBooleanArray();
+		String expected = getExpectedBooleanArrayResult(bools, separator);
+		String phrase = Phrase.from("{boolean_array}").putArray("boolean_array", bools, separator).formatString();
+		printResult("booleanArrayTest()", phrase, expected);
+		assertEquals(phrase, expected);
+	}
+
+	@Test
+	public void floatArrayTest() {
+		float[] floats = getRandomFloatArray();
+		String expected = getExpectedFloatArrayResult(floats, separator);
+		String phrase = Phrase.from("{float_array}").putArray("float_array", floats, separator).formatString();
+		printResult("floatArrayTest()", phrase, expected);
+		assertEquals(phrase, expected);
+	}
+
+	@Test
+	public void doubleArrayTest() {
+		double[] doubles = getRandomDoubleArray();
+		String expected = getExpectedDoubleArrayResult(doubles, separator);
+		String phrase = Phrase.from("{double_array}").putArray("double_array", doubles, separator).formatString();
+		printResult("doubleArrayTest()", phrase, expected);
+		assertEquals(phrase, expected);
 	}
 
 }
